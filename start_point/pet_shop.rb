c9dba485b9f38c2db538d3e5d8670df65731a88f
Russell Williams <russellwilliamscc@gmail.com>
2016-06-10 15:52:23 +0100

    adds breed test

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -2,10 +2,39 @@ def pet_shop_name(pet_shop)
   return pet_shop[:name]
 end
 
-def total_cash(monies)
-  return monies[:admin][:total_cash]
+def total_cash(total)
+  return total[:admin][:total_cash]
 end
 
-def test_add_or_remove_cash(monies)
-  return monies
+def add_or_remove_cash(total, cash)
+  return total[:admin][:total_cash] += cash
 end
+
+def pets_sold(pet)
+  return pet[:admin][:pets_sold]
+end
+
+def increase_pets_sold(pets_sold, sold)
+  return pets_sold[:admin][:pets_sold] += sold
+end
+
+
+def stock_count(count)
+  return count[:pets].size
+end
+
+
+def pets_by_breed(pet_shop, breed)
+  result = []
+  for pet in pet_shop[:pets]
+  if 
+    pet[:breed] == breed
+    result.push pet
+  end
+end
+
+  return result
+end
+
+
+

95e2c0effba7504dea1dec81f125ad49d0071f4a
Russell Williams <russellwilliamscc@gmail.com>
2016-06-10 11:41:59 +0100

    initial commit

---
+++ start_point/pet_shop.rb
@@ -0,0 +1,11 @@
+def pet_shop_name(pet_shop)
+  return pet_shop[:name]
+end
+
+def total_cash(monies)
+  return monies[:admin][:total_cash]
+end
+
+def test_add_or_remove_cash(monies)
+  return monies
+end
